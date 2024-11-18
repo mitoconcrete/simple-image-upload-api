6327f551d911f8f7941539cd6af863c7305daec1
mitoconcrete <xogns98@gmail.com>
2024-11-18 11:50:10 +0900

    feat: lifespan을 사용해, create table 1회만 할 수 있도록 설정.

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.api.v1 import images
@@ -5,13 +6,16 @@ from app.exception.common import exceptions as common_exceptions
 from app.exception.image import exceptions as image_exceptions
 from app.util.init_db import create_tables
 
-app = FastAPI()
+@asynccontextmanager
+async def lifecycle(app: FastAPI):
+    create_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifecycle)
 
 # Include routers
 app.include_router(images.router, prefix='/api/v1/images', tags=['Images'])
-
-# create_tables
-create_tables()
 
 # Include exception handlers
 [app.add_exception_handler(exception, handler) for exception, handler in common_exceptions + image_exceptions]
