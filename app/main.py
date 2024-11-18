a20de27e446b2b94c36b72183f76f4f0212ec163
mitoconcrete <xogns98@gmail.com>
2024-11-18 11:51:23 +0900

    fix: lint, format 적용

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api.v1 import images
 from app.exception.common import exceptions as common_exceptions
 from app.exception.image import exceptions as image_exceptions
 from app.util.init_db import create_tables
+
 
 @asynccontextmanager
 async def lifecycle(app: FastAPI):
